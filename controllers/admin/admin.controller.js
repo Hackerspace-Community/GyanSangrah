d3c08896ea0c1dce9d85fcc69e29bb8d712ec833
Harshit Kr Vishwakarma <vharshitkr01gmail.com>
2022-07-31 01:37:47 +0530

    Admin can now approve/edit/delete user

--- controllers/admin/admin.controller.js
+++ controllers/admin/admin.controller.js
@@ -1,32 +1,35 @@
 /**
  * Node modules.
  */
- const validator = require("validator");
+const validator = require("validator");
 
- /**
-  * Model imports.
-  */
- const User = require("../../models/user.model.js");
- const Category = require("../../models/admin.category.model.js");
- 
- 
- /**
-  * Utils.
-  */
- const catchAsync = require("../../utils/catchAsync.js");
+/**
+ * Model imports.
+ */
+const User = require("../../models/user.model.js");
+const Category = require("../../models/admin.category.model.js");
+
+
+/**
+ * Utils.
+ */
+const catchAsync = require("../../utils/catchAsync.js");
 
 
 /**
  * @description - Get all contributors request.
  */
- module.exports.allContributorRequest = catchAsync(async (req, res) => {
-    const users = await User.find()
+module.exports.allContributorRequest = catchAsync(async (req, res) => {
+    // Find all users who requested to become a contributor
+    const users = await User.find({
+        contributorStatus: "pending"
+    })
 
-    if(!users) {
+    if (!users) {
         req.flash("error", "No Users found");
         return res.redirect("/");
     }
-    
+
     res.render("admin/contributorManagement", {
         users,
     });
@@ -34,7 +37,7 @@
 
 
 module.exports.contributorRequestApprove = catchAsync(async (req, res) => {
-  
+
     const {
         id
     } = req.params;
@@ -42,41 +45,37 @@ module.exports.contributorRequestApprove = catchAsync(async (req, res) => {
 
 
     User.findByIdAndUpdate(id, { role: 'ROLE_CONTRIBUTOR' },
-                            function (err, docs) {
-    if (err){
-        console.log(err)
-    }
-    else{
-        return res.redirect("/admin/contributorManagement");
-    }
+        function (err, docs) {
+            if (err) {
+                console.log(err)
+            }
+            else {
+                return res.redirect("/admin/contributorManagement");
+            }
+        });
+
+
+
+
+
 });
 
 
- 
-    
- 
-});
 
 
-
-
-
+/**
+ * @description - Delete user profile.
+ */
 module.exports.contributorRequestDelete = catchAsync(async (req, res) => {
-  
+
     const {
         id
     } = req.params;
-    console.log(id);
 
-    User.findByIdAndRemove(req.params.id, function(err){
-        if(err){
-            console.log(err)
-        } else {
-            return res.redirect("/admin/contributorManagement");
-        }
-     });
-    
- 
+    await User.findByIdAndRemove(id);
+
+    req.flash("success", "User deleted successfully");
+    return res.redirect("/admin/contributorManagement");
 });
 
 
@@ -87,11 +86,11 @@ module.exports.contributorRequestDelete = catchAsync(async (req, res) => {
 module.exports.addCategory = catchAsync(async (req, res) => {
     const category = await Category.find()
 
-    if(!category) {
+    if (!category) {
         req.flash("error", "No Users found");
         return res.redirect("/");
     }
-    
+
     res.render("admin/category", {
         category,
     });
@@ -102,21 +101,19 @@ module.exports.categoryAdd = catchAsync(async (req, res) => {
         title
     } = req.body;
 
-    if(!title) {
+    if (!title) {
         req.flash("error", "Please fill in all fields");
         return res.redirect("/admin/category");
     }
-    
+
     const category = new Category({
-        title, status:"ACTIVE",
+        title, status: "ACTIVE",
     });
     await category.save();
     return res.redirect("/admin/category");
 
-
-
-
 });
+
 module.exports.categoryDelete = catchAsync(async (req, res) => {
 
     const {
@@ -124,13 +121,13 @@ module.exports.categoryDelete = catchAsync(async (req, res) => {
     } = req.params;
     console.log(id);
 
-    Category.findByIdAndRemove(req.params.id, function(err){
-        if(err){
+    Category.findByIdAndRemove(req.params.id, function (err) {
+        if (err) {
             console.log(err)
         } else {
             return res.redirect("/admin/category");
         }
-     });
+    });
 
 
 });
